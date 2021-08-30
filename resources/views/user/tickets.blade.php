3040c0fcbaf14679cba9f005966e3ed7bd89072e
SauravRaj <Souravraj@justconsult.us>
2021-08-30 21:41:30 +0530

    minor update

--- resources/views/user/tickets.blade.php
+++ resources/views/user/tickets.blade.php
@@ -33,7 +33,7 @@
     <div class="topnav">
         <a href="{{ route('user.index') }}">Dashboard</a>
         <a href="{{ route('user.subscription', $user->id) }}">Subscriptions</a>
-        <a href="{{ route('user.progress', $user->id) }}"> Progress </a>
+        {{-- <a href="{{ route('user.progress', $user->id) }}"> Progress </a> --}}
         <a href="{{ route('user.package', $user->id) }}">Package</a>
         <a href="{{ route('user.ebook', $user->id) }}">My Ebooks</a>
         <a href="{{ route('user.report', $user->id) }}">My Reports</a>
